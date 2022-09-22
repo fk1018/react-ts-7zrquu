2bdbe201d0fe7e0dab0d703ebf78903ccf93ec47
fk1018 <fk1018@users.noreply.github.com>
2022-09-22 11:37:00 -0500

    2 files modified

--- components/FlyOut.tsx
+++ components/FlyOut.tsx
@@ -1,41 +1,62 @@
 import React from 'react';
 import { useListingsContext } from '../context/ListingsProvider';
 
-export default function Input(props) {
-  const listings = useListingsContext();
+const FlyoutContext = React.createContext(null);
+const useFlyoutContext = () => React.useContext(FlyoutContext);
 
+function Flyout(props) {
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState('');
   const toggle = React.useCallback(() => setOpen((state) => !state), []);
 
   return (
-    <div className="flyout">
-      <input
-        onFocus={toggle}
-        onBlur={toggle}
-        className="flyout-input"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        placeholder="Enter an address, city ,or ZIP code"
-        {...props}
-      />
-      {open && (
-        <div className="flyout-list">
-          <ul>
-            {listings.map((listing) => (
-              <li
-                key={listing.id}
-                onMouseDown={() => {
-                  setValue(listing.name);
-                }}
-                className="flyout-list-item"
-              >
-                {listing.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <FlyoutContext.Provider value={{ open, toggle, value, setValue }}>
+      {props.children}
+    </FlyoutContext.Provider>
   );
 }
+
+function Input(props) {
+  const { toggle, value, setValue } = useFlyoutContext();
+  return (
+    <input
+      onFocus={toggle}
+      onBlur={toggle}
+      className="flyout-input"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      placeholder="Enter an address, city ,or ZIP code"
+      {...props}
+    />
+  );
+}
+
+function List(props) {
+  return (
+    open && (
+      <div className="flyout-list">
+        <ul>{props.children}</ul>
+      </div>
+    )
+  );
+}
+
+function Item(props) {
+  const { setValue } = useFlyoutContext();
+  return (
+    <li
+      onMouseDown={() => {
+        setValue(props.value);
+      }}
+      className="flyout-list-item"
+    >
+      {props.children}
+    </li>
+  );
+}
+
+Flyout.Input = Input;
+Flyout.List = List;
+Flyout.Item = Item;
+
+export { Flyout };
